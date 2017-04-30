197379ebb470527232364b8a6ae848964ddedd3a
hoinarut <tudor.hoinaru@gmail.com>
2017-04-30 10:58:48 +0300

    Adding idea project Adding named scopes

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,10 +1,9 @@
 class DemoController < ApplicationController
   def index
-    
   end
 
   def hello
-    @array = [1,2,3,4,5]
+    @array = [1, 2, 3, 4, 5]
     @id = params[:id].to_i
     @page = params[:page].to_i
   end
