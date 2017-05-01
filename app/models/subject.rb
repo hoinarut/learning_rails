62cece15eeb05456882616663238e60c8055b63f
hoinarut <tudor.hoinaru@gmail.com>
2017-05-01 06:12:31 +0300

    Added relations

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,5 +1,8 @@
 class Subject < ApplicationRecord
 
+  has_many :pages
+  has_many :sections
+  
   scope :visible, lambda {where(:visible => true)}
   scope :invisible, lambda {where(:visible => false)}
   scope :sorted, lambda {order("position ASC")}
