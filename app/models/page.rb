1ff51cedc8b181ad6dfbc45d0cd5082400ede769
hoinarut <tudor.hoinaru@gmail.com>
2017-05-01 08:20:12 +0300

    Added associations; added Subjects controller

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,5 @@
 class Page < ApplicationRecord
 
-    belongs_to :subject
-
+    belongs_to :subject, { :optional => true }
+    has_and_belongs_to_many :admin_users
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'subjects/index'
+
+  get 'subjects/show'
+
+  get 'subjects/new'
+
+  get 'subjects/edit'
+
+  get 'subjects/delete'
+
   #get 'demo/index'
   #match "ridiculous", :to => "demo#index"
   root :to => "demo#index"
