7b6aab4d98ff0cc93f8f3407a0cc1ef2d2de07a5
hoinarut <tudor.hoinaru@gmail.com>
2017-05-04 09:02:21 +0300

    Added pages and sections controllers and related

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -14,6 +14,7 @@ class SubjectsController < ApplicationController
   def create
     @subject = Subject.new(subject_params)
     if(@subject.save)
+      flash[:notice] = "Subject created successfully."
       redirect_to(subjects_path)
     else
       render('new')
@@ -21,15 +22,32 @@ class SubjectsController < ApplicationController
   end
 
   def edit
+    @subject = Subject.find(params[:id])
   end
 
   def update
+    # Find a new object using form parameters
+    @subject = Subject.find(params[:id])
+    # Update the object
+    if @subject.update_attributes(subject_params)
+      # If save succeeds, redirect to the show action
+      flash[:notice] = "Subject updated successfully."
+      redirect_to(subject_path(@subject))
+    else
+      # If save fails, redisplay the form so user can fix problems
+      render('edit')
+    end
   end
 
   def delete
+    @subject = Subject.find(params[:id])
   end
 
   def destroy
+    @subject = Subject.find(params[:id])
+    @subject.destroy
+    flash[:notice] = "Subject '#{@subject.name}' destroyed successfully."
+    redirect_to(subjects_path)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
-   
+     
   root 'demo#index'
 
   resources :subjects do
+    member do
+      get :delete
+    end
+  end
+
+  resources :pages do
+    member do
+      get :delete
+    end
+  end
+
+  resources :sections do
     member do
       get :delete
     end
