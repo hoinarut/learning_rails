b0141e8c5bcd721ac432ea2ea62dd0665fb85ebe
hoinarut <tudor.hoinaru@gmail.com>
2017-05-09 11:54:32 +0300

    Gemfile.lock

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20170508090945) do
 
-  create_table "admin_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "admin_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "first_name",      limit: 25
     t.string   "last_name",       limit: 50
     t.string   "email",           limit: 100, default: "", null: false
@@ -23,13 +23,13 @@ ActiveRecord::Schema.define(version: 20170508090945) do
     t.index ["username"], name: "index_admin_users_on_username", using: :btree
   end
 
-  create_table "admin_users_pages", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "admin_users_pages", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.integer "admin_user_id"
     t.integer "page_id"
     t.index ["admin_user_id", "page_id"], name: "index_admin_users_pages_on_admin_user_id_and_page_id", using: :btree
   end
 
-  create_table "pages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "pages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.integer  "subject_id"
     t.string   "name"
     t.string   "permalink"
@@ -41,7 +41,7 @@ ActiveRecord::Schema.define(version: 20170508090945) do
     t.index ["subject_id"], name: "index_pages_on_subject_id", using: :btree
   end
 
-  create_table "section_edits", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "section_edits", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.integer  "admin_user_id"
     t.integer  "section_id"
     t.string   "summary"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 20170508090945) do
     t.index ["admin_user_id", "section_id"], name: "index_section_edits_on_admin_user_id_and_section_id", using: :btree
   end
 
-  create_table "sections", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "sections", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.integer  "page_id"
     t.string   "name"
     t.integer  "position"
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 20170508090945) do
     t.index ["page_id"], name: "index_sections_on_page_id", using: :btree
   end
 
-  create_table "subjects", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "subjects", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "name"
     t.integer  "position"
     t.boolean  "visible",    default: false
